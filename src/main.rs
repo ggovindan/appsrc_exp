3dcb21e6330e336e7ee9ccd875c2dc6f7ba8a624
Guru Govindan <gxguru@gmail.com>
2024-01-04 16:15:08 -0800

    fdsink version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use gst::prelude::*;
 use anyhow::Error;
 use derive_more::{Display, Error};
-use gst::BufferFlags;
-use std::io::{BufReader, Read};
+use std::io::{Read};
 use std::fs::File;
 use std::{thread, time};
+use std::os::fd::{AsRawFd, RawFd};
+use std::io::prelude::*;
+use std::process::exit;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
+use lazy_static::lazy_static;
+
+use tracing::{info, warn, error, Level, debug};
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::prelude::*;
 
 #[cfg(not(target_os = "macos"))]
 pub fn run<T, F: FnOnce() -> T + Send + 'static>(main: F) -> T
@@ -55,6 +65,9 @@ pub fn run<T, F: FnOnce() -> T + Send + 'static>(main: F) -> T
     }
 }
 
+lazy_static! {
+    static ref SHARED_FILE: Arc<Mutex<Option<File>>> = Arc::new(Mutex::new(None));
+}
 
 #[derive(Debug, Display, Error)]
 #[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
@@ -88,7 +101,7 @@ fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
                         .map(|s| String::from(s.path_string()))
                         .unwrap_or_else(|| String::from("None")),
                     error: err.error().to_string(),
-                    debug: Some("".to_string()),
+                    debug: Some(err.debug().unwrap().to_string()),
                     source: err.error(),
                 }
                     .into());
@@ -103,93 +116,220 @@ fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
 const WIDTH: usize = 320;
 const HEIGHT: usize = 240;
 
-// gst-launch-1.0 videotestsrc ! videoconvert ! avenc_h264_videotoolbox ! mpegtsmux ! filesink location="dip.ts"
+#[derive(Debug)]
+pub struct CustomEOSEvent {
+    pub send_eos: bool,
+}
+
+impl CustomEOSEvent {
+    const EVENT_NAME: &'static str = "custom-eos-event";
+
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new(send_eos: bool) -> gst::Event {
+        let s = gst::Structure::builder(Self::EVENT_NAME)
+            .field("send_eos", send_eos)
+            .build();
+        gst::event::CustomDownstream::new(s)
+    }
+
+    pub fn parse(ev: &gst::EventRef) -> Option<CustomEOSEvent> {
+        match ev.view() {
+            gst::EventView::CustomDownstream(e) => {
+                let s = match e.structure() {
+                    Some(s) if s.name() == Self::EVENT_NAME => s,
+                    _ => return None, // No structure in this event, or the name didn't match
+                };
+
+                let send_eos = s.get::<bool>("send_eos").unwrap();
+                Some(CustomEOSEvent { send_eos })
+            }
+            _ => None, // Not a custom event
+        }
+    }
+}
+
+
+
 fn create_pipeline() -> Result<gst::Pipeline, Error> {
     gst::init()?;
 
-    let pipeline = gst::Pipeline::default();
+    // let pipeline = gst::parse_launch(format!("appsrc name=appsrc do-timestamp=true ! video/mpegts,systemstream=true ! tsdemux name=demux \
+    // demux. ! h264parse ! queue ! avdec_h264  ! videoconvert ! avenc_h264_videotoolbox ! mpegtsmux name=mux ! appsink name=appsink").as_str())
+    let pipeline = gst::parse_launch(format!("appsrc name=appsrc do-timestamp=true ! video/mpegts,systemstream=true ! tsdemux name=demux \
+    demux. ! h264parse ! queue ! avdec_h264  ! videoconvert ! avenc_h264_videotoolbox ! mpegtsmux name=mux ! fdsink name=filesink demux. ! queue ! aacparse ! mux.").as_str())
+        .unwrap()
+        .downcast::<gst::Pipeline>()
+        .unwrap();
 
-    let video_info = &gst::Caps::builder("video/mpegts")
-        .field("systemstream", true)
-        .build();
+    let pipeline_weak = pipeline.downgrade();
 
-    let appsrc = gst_app::AppSrc::builder()
-        .caps(video_info)
-        .format(gst::Format::Time)
-        .is_live(false)
-        .build();
+    let appsrc = pipeline.by_name("appsrc")
+        .unwrap()
+        .downcast::<gst_app::AppSrc>()
+        .unwrap();
 
-    // gst-launch-1.0 filesrc location=dip.ts ! tsdemux name=demux demux. ! h264parse ! decodebin ! videoconvert ! autovideosink
-    // gst-launch-1.0 filesrc location=dip.ts ! tsdemux name=demux demux. ! h264parse ! mpegtsmux ! filesink location=pp.ts
-    let tsdemux = gst::ElementFactory::make("tsdemux").build()?;
-    let h264parse = gst::ElementFactory::make("h264parse").build()?;
-    let mpegtsmux = gst::ElementFactory::make("mpegtsmux").build()?;
-    let filesink = gst::ElementFactory::make("multifilesink").build()?;
-    //let filesink = gst::ElementFactory::make("filesink").build()?;
-    filesink.set_property("location", "%d.ts");
-    //filesink.set_property("post-messages", true);
-    filesink.set_property_from_str("next-file", "buffer");
+    let mut outputfile = File::create("output.ts").expect("unable tot create file");
 
-    // moved out of lambda to avoid move closures
-    let video_sink_pad = h264parse.static_pad("sink").expect("could not get sink pad from h264parse");
+    let mut i = 1;
+    appsrc.set_callbacks(
+        gst_app::AppSrcCallbacks::builder()
+            .need_data(move |appsrc, _| {
+                info!("getting the file");
+                i+=1;
 
-    pipeline.add_many(&[appsrc.upcast_ref(), &tsdemux, h264parse.upcast_ref(), &mpegtsmux, &filesink])?;
-    gst::Element::link_many(&[appsrc.upcast_ref(), &tsdemux])?;
+                let mut buffer_new = gst::Buffer::new();
+                {
+                    let buffer = buffer_new.get_mut().unwrap();
+                    buffer.set_size(0);  // Set size to 0 for an empty buffer
+                }
 
-    tsdemux.connect_pad_added(move |_src, src_pad| {
-        let is_video = if src_pad.name().starts_with("video") {
-            true
-        } else {
-            false
-        };
+                // Push the buffer into the pipeline
+                appsrc.push_buffer(buffer_new).unwrap();
 
-        let connect_demux = || -> Result<(), Error> {
-            src_pad.link(&video_sink_pad).expect("failed to link tsdemux.video->h264parse.sink");
-            println!("linked tsdemux->h264parse");
-            Ok(())
-        };
 
-        if is_video {
-            match connect_demux() {
-                Ok(_) => println!("tsdemux->h264 connected"),
-                Err(e) => println!("could not connect tsdemux->h264parse e:{}", e),
+                if let Some(pipeline) = pipeline_weak.upgrade() {
+                    let ev = CustomEOSEvent::new(true);
+                    if !pipeline.send_event(ev) {
+                        println!("warning! failed to send eos event")
+                    } else {
+                        println!("appsrc sending event on pipeline!")
+                    }
+                }
+
+
+                //let mut file = File::create(format!("video{}.ts", i)).unwrap();
+                // if let Some(pipeline) = pipeline_weak.upgrade() {
+                //     let mut file_lock = SHARED_FILE.lock().unwrap();
+                //     *file_lock = Some(File::create(format!("{}.ts", i)).unwrap());
+                //     if let Some(ref file) = *file_lock {
+                //         // Obtain the raw file descriptor
+                //         let raw_fd: RawFd = file.as_raw_fd();
+                //         info!("Raw file descriptor: {}", raw_fd);
+                //         let elem = pipeline.by_name("filesink").unwrap();
+                //         elem.set_property("fd", raw_fd);
+                //
+                //     } else {
+                //         // Handle the case where the file is not yet created or is None
+                //         info!("No file available.");
+                //     }
+                // }
+
+                let mut file = File::open("./sample/test_1620_h264.ts").unwrap();
+                let mut buffer = Vec::new();
+                match file.read_to_end(&mut buffer) {
+                    Ok(_) => { info!(cam_id = "snx-c2ahme", "finished reading bytes from file len={}", buffer.len());
+                    }
+                    Err(e) => {
+                        println!("error reading file: {}", e);
+                    }
+                };
+                //let reader = BufReader::new(file);
+                //let file_buf = reader.buffer();
+                let gst_buffer = gst::Buffer::from_slice(buffer);
+                println!("buffer size of teh generated gst_buffer={}", gst_buffer.size());
+                let _ = appsrc.push_buffer(gst_buffer);
+
+                println!("--------sleeping for 1 sec-----------");
+                thread::sleep(Duration::from_secs(1));
+
+                //appsrc.end_of_stream();
+
+            }).build(),
+    );
+
+    let fdsink = pipeline.by_name("filesink").unwrap();
+        // .downcast::<gst_app::AppSink>()
+        // .unwrap();
+
+    let pipeline_weak = pipeline.downgrade();
+
+    // first set a pad probe
+    let sinkpad = fdsink.static_pad("sink").unwrap();
+
+    let mut file_lock = SHARED_FILE.lock().unwrap();
+    *file_lock = Some(File::create(format!("{}.ts", 1)).unwrap());
+    if let Some(ref file) = *file_lock {
+        // Obtain the raw file descriptor
+        let raw_fd: RawFd = file.as_raw_fd();
+        println!("Raw file descriptor: {}", raw_fd);
+        fdsink.set_property("fd", raw_fd);
+    } else {
+        // Handle the case where the file is not yet created or is None
+        error!("No file available.");
+    }
+
+
+    let event_counter = AtomicUsize::new(0);
+
+
+    sinkpad.add_probe(gst::PadProbeType::EVENT_DOWNSTREAM, move |x, probe_info| {
+        match probe_info.data {
+            Some(gst::PadProbeData::Event(ref ev))
+            if ev.type_() == gst::EventType::CustomDownstream => {
+                if let Some(custom_event) = CustomEOSEvent::parse(ev) {
+                    if let Some(pipeline) = pipeline_weak.upgrade() {
+                        println!("closing file!! received custom event on appsink i={}", event_counter.load(Ordering::SeqCst));
+                        event_counter.fetch_add(1, Ordering::SeqCst);
+                        // we have received the event.. change the file descriptor
+                        let mut file_lock = SHARED_FILE.lock().unwrap();
+                        *file_lock = Some(File::create(format!("{}.ts", event_counter.load(Ordering::SeqCst))).unwrap());
+                        if let Some(ref file) = *file_lock {
+                            // Obtain the raw file descriptor
+                            let raw_fd: RawFd = file.as_raw_fd();
+                            println!("Raw file descriptor: {}", raw_fd);
+                            let elem = pipeline.by_name("filesink").unwrap();
+                            elem.set_property("fd", raw_fd);
+                        } else {
+                            // Handle the case where the file is not yet created or is None
+                            error!("No file available.");
+                        }
+                    }
+                }
             }
+            _ => (),
         }
+        gst::PadProbeReturn::Ok
     });
 
 
-    gst::Element::link_many(&[&h264parse.upcast_ref(), &mpegtsmux, &filesink])?;
-
-     appsrc.set_callbacks(
-         gst_app::AppSrcCallbacks::builder()
-             .need_data(move |appsrc, _| {
-                 println!("getting the file");
-                 let mut file = File::open("./sample/test_1620.ts").unwrap();
-                 let mut buffer = Vec::new();
-                 match file.read_to_end(&mut buffer) {
-                     Ok(_) => {
-                         println!("finished reading bytes from file len={}", buffer.len());
-                     }
-                     Err(e) => {
-                         println!("error reading file: {}", e);
-                     }
-                 };
-                 //let reader = BufReader::new(file);
-                 //let file_buf = reader.buffer();
-                 let gst_buffer = gst::Buffer::from_slice(buffer);
-                 println!("buffer size of teh generated gst_buffer={}", gst_buffer.size());
-                 appsrc.end_of_stream();
-                 let _ = appsrc.push_buffer(gst_buffer);
-                 println!("sleeping for 10 secs");
-                 thread::sleep(time::Duration::from_secs(10));
-             }).build(),
-     );
+    // appsink.set_callbacks(
+    //     gst_app::AppSinkCallbacks::builder()
+    //         .new_sample(move |appsink| {
+    //             let sample = appsink.pull_sample().map_err(|_| gst::FlowError::Eos)?;
+    //             let buffer = sample.buffer().ok_or_else(|| {
+    //                     println!("Failed to get buffer from appsink");
+    //                 gst::FlowError::Error
+    //             })?;
+    //
+    //             let map = buffer.map_readable().map_err(|_| {
+    //                     println!("Failed to map buffer readable");
+    //                 gst::FlowError::Error
+    //             })?;
+    //
+    //             outputfile.write_all(map.as_slice()).expect("unable to write the file");
+    //
+    //             Ok(gst::FlowSuccess::Ok)
+    //         })
+    //         .build()
+    // );
 
     Ok(pipeline)
 
 }
 
 fn appsrc_main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer()
+            // Enable JSON formatter
+            .json()
+            // Include file name, line number, target, etc.
+            .with_span_list(false)
+            .with_span_events(FmtSpan::CLOSE)
+            .with_file(true)
+            .with_line_number(true)
+            .with_target(true)
+        )
+        .init();
     match create_pipeline().and_then(main_loop) {
         Ok(r) => r,
         Err(e) => println!("Error! {}", e),
